e221533dd8ec00da0e3f2a38f352e386de8464e6
Sherzod Erkinov <sherzod.erkinov036@gmail.com>
2020-08-24 18:56:29 +0500

    Создание form блока

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+    $("nav.tabs").delegate("li", "click", function () {
+        var li = $(this);
+
+        if (!li.is(".selected")) {
+            li.addClass("selected").siblings("li").removeClass("selected");
+            $("nav.tabs").siblings("div").hide().eq(li.index()).show();
+        }
+    });
+
+    $(".tabs ul li a").on("click", function () {
+        $(".tabs ul li a").removeClass("tab-active");
+        $(this).addClass("tab-active");
+    });
+
+    $(".datepicker").datepicker({
+        format: "mm/dd/yyyy",
+        startDate: "-3d",
+    });
+});
