b2b72f1358760dd1094d5e87bda640450ac193bd
Sherzod Erkinov <sherzod.erkinov036@gmail.com>
2020-09-08 03:05:56 +0500

    responsive + анимация

--- js/script.js
+++ js/script.js
@@ -13,8 +13,40 @@ $(document).ready(function () {
         $(this).addClass("tab-active");
     });
 
-    $(".datepicker").datepicker({
-        format: "mm/dd/yyyy",
-        startDate: "-3d",
+    $(".hamburger").on("click", function () {
+        $(this).toggleClass("is-active");
+        $(".header__navigation").slideToggle().toggleClass("d-block");
+    });
+
+    // let hero = $(".hero");
+    // $(window).scroll(function () {
+    //     hero.css({ backgroundPosition: "0 " + -window.scrollY + "px" });
+    // });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from(".hero__text", {
+        scrollTrigger: ".hero",
+        y: "100%",
+        duration: 2,
+        opacity: 0,
+    });
+    gsap.from(".hero__button", {
+        scrollTrigger: ".hero",
+        y: "100%",
+        duration: 2,
+        opacity: 0,
+    });
+    gsap.from(".offer__title", {
+        scrollTrigger: ".offer__title",
+        y: "100%",
+        duration: 2,
+        opacity: 0,
+    });
+    gsap.from(".working__title", {
+        scrollTrigger: ".working",
+        y: "100%",
+        duration: 2,
+        opacity: 0,
     });
 });
