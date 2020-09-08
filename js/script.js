15f6f117f1a41f059d1ed7f462c215f72c51ee93
Sherzod Erkinov <sherzod.erkinov036@gmail.com>
2020-09-09 04:59:31 +0500

    Сайт готов кроме parallax анимации

--- js/script.js
+++ js/script.js
@@ -18,10 +18,52 @@ $(document).ready(function () {
         $(".header__navigation").slideToggle().toggleClass("d-block");
     });
 
-    // let hero = $(".hero");
-    // $(window).scroll(function () {
-    //     hero.css({ backgroundPosition: "0 " + -window.scrollY + "px" });
-    // });
+    $(".single-tour__slider-bottom-imgs").slick({
+        slidesToShow: 7,
+        prevArrow:
+            '<img class="prev-arrow" src="../images/slider/arrow-left.svg">',
+        nextArrow:
+            '<img class="next-arrow" src="../images/slider/arrow-right.svg">',
+        responsive: [
+            {
+                breakpoint: 995,
+                settings: {
+                    slidesToShow: 6,
+                },
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+        ],
+    });
+
+    $(".open_popup").click(function () {
+        $(".single-tour__popup").fadeIn().css({ display: "flex" });
+    });
+
+    $(".single-tour__close-icon").click(function () {
+        $(".single-tour__popup").fadeOut();
+    });
+
+    let gallery = $(".single-tour__slider-bottom-img img");
+
+    gallery.on("click", function () {
+        let currentImage = $(this).attr("src");
+        $(".single-tour__slider-top-img img")
+            .fadeOut(400, function () {
+                $(".single-tour__slider-top-img img").attr("src", currentImage);
+                $(".single-tour__slider-top-img img").attr("srcset", "");
+            })
+            .fadeIn();
+    });
+
+    gallery.on("click", function () {
+        gallery.removeClass("slider-border");
+        $(this).addClass("slider-border");
+    });
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -48,5 +90,10 @@ $(document).ready(function () {
         y: "100%",
         duration: 2,
         opacity: 0,
+    });
+
+    $(".header__navigation ul li a").on("click", function () {
+        $(".header__navigation ul li a").removeClass("header-border");
+        $(this).addClass("header-border");
     });
 });
